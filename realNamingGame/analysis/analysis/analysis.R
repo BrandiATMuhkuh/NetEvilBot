4b2688c6821be14988b9ce7d325f29f7e1408f47
Juergen Brandstetter <jurgen@osnova.nz>
2017-05-05 16:15:57 +1200

    added sd and mean to analysis

--- realNamingGame/analysis/analysis/analysis.R
+++ realNamingGame/analysis/analysis/analysis.R
@@ -16,10 +16,42 @@ catchSet = subset(classroom, classroom$Robots. > 1 & (centrality == '"random"' |
 t.test(catchSet$stat_1000_robot_percent ~ catchSet$centrality)
 
 
+catchSet = subset(classroom, classroom$Robots. > 1 & (centrality == '"betweenness-centrality"' | centrality == '"page-rank"'))
+t.test(catchSet$stat_1000_robot_percent ~ catchSet$centrality)
+
+catchSet = subset(classroom, classroom$Robots. > 1 & (centrality == '"betweenness-centrality"' | centrality == '"closeness-centrality"'))
+t.test(catchSet$stat_1000_robot_percent ~ catchSet$centrality)
+
+
 #0\% (only humans), 3\% , 6\% , 9\% , 11\% , 26\% , 44\% , 85\%.
 catchSet = subset(classroom, (centrality == '"closeness-centrality"' | centrality == '"betweenness-centrality"'  | centrality == '"page-rank"'))
 summary(lm(stat_5000_robot_percent ~ centrality ,data=catchSet))
 
 catchSet = subset(classroom, classroom$Robots. > 1)
 boxplot(catchSet$stat_1000_robot_percent ~ catchSet$centrality*catchSet$Robots., col=(c("blue","yellow", "orange",  "green")), outline = FALSE)
+
+
+#anova centrality  ~ stat_5000_robot_percent
+classroom <- read.csv("classroom_full.csv")
+plot(classroom$stat_5000_robot_percent ~ classroom$centrality)
+mod1 = lm(classroom$stat_5000_robot_percent ~ classroom$centrality)
+summary(mod1)
+
+
+#calc the averages
+print("betweenness-centrality")
+summary(subset(classroom, classroom$Robots. > 1 & (centrality == '"betweenness-centrality"'))$stat_5000_robot_percent)
+sd(subset(classroom, classroom$Robots. > 1 & (centrality == '"betweenness-centrality"'))$stat_5000_robot_percent)
+
+print("closeness-centrality")
+summary(subset(classroom, classroom$Robots. > 1 & (centrality == '"closeness-centrality"'))$stat_5000_robot_percent)
+sd(subset(classroom, classroom$Robots. > 1 & (centrality == '"closeness-centrality"'))$stat_5000_robot_percent)
+
+print("page-rank")
+summary(subset(classroom, classroom$Robots. > 1 & (centrality == '"page-rank"'))$stat_5000_robot_percent)
+sd(subset(classroom, classroom$Robots. > 1 & (centrality == '"page-rank"'))$stat_5000_robot_percent)
+
+print("random")
+summary(subset(classroom, classroom$Robots. > 1 & (centrality == '"random"'))$stat_5000_robot_percent)
+sd(subset(classroom, classroom$Robots. > 1 & (centrality == '"random"'))$stat_5000_robot_percent)
 
--- realNamingGame/analysis/analysis/offsetAnalysis.R
+++ realNamingGame/analysis/analysis/offsetAnalysis.R
@@ -1,3 +1,4 @@
+
 off <- read.csv("robotOffset_full.csv")
 
 off = subset(off, RobotOffset > 49)
@@ -10,4 +11,33 @@ anova(lm(off$stat_1000_robot_percent ~ off$RobotOffset))
 a = lm(stat_2500_robot_percent ~ RobotOffset, data=subset(off, Robots. == 3))
 b = lm(stat_2500_robot_percent ~ RobotOffset, data=subset(off, Robots. == 6))
 c = lm(stat_2500_robot_percent ~ RobotOffset, data=subset(off, Robots. == 9))
+anova(a,b,c)
 summary(anova(a,b,c))
+
+
+data70 = subset(subset(off, RobotOffset > 70), Robots. == 9)
+data50 = subset(subset(off, RobotOffset > 50), Robots. == 9)
+data30 = subset(subset(off, RobotOffset > 30), Robots. == 9)
+data10 = subset(subset(off, RobotOffset > 10), Robots. == 9)
+lm70 = lm(stat_2500_robot_percent ~ RobotOffset, data70)
+lm50 = lm(stat_2500_robot_percent ~ RobotOffset, data50)
+lm30 = lm(stat_2500_robot_percent ~ RobotOffset, data30)
+lm10 = lm(stat_2500_robot_percent ~ RobotOffset, data10)
+anova(lm70,lm50,lm30)
+
+
+
+#select only between centrality
+data <- read.csv("robotOffset_full.csv")
+data = subset(data,  centrality == '"betweenness-centrality"')
+#select only data with same mount of robots
+data = subset(data, Robots. == 11)
+data$RobotOffset = factor(data$RobotOffset)
+#compare the dataset on it's robot offset level
+model = lm(stat_5000_robot_percent ~ RobotOffset ,data=data)
+summary(model)
+ano = anova(model)
+summary(ano)
+
+
+
